af65081097a92637f1798b6fd9fa555e6b63f4ac
Pavel <pavelislam729@gmail.com>
2021-10-18 03:01:09 +0100

    version 1

---
+++ app2.js
@@ -0,0 +1,37 @@
+const timeLeft = document.getElementById('time-left');
+const birthday = new Date('10/23/2021');
+const sec = 1000;
+const min = sec*60;
+const hour = min*60;
+const day = hour*24;
+let timerId
+
+
+function countDown(){
+    const today = new Date();
+    const timeSpan = (birthday - today);
+    console.log(timeSpan);
+
+    if (timeSpan <= -day){
+        timeLeft.innerHTML = "Hope You had a great time !";
+        clearInterval(timerId);
+        return;
+    }
+
+    else if (timeSpan <= 0){
+        timeLeft.innerHTML = "<span>Happy</span> <span>Birthday</span> <span>R</span><span>i</span><span>c</span><span>h</span><span>i</span> <span>!!!</span> ";
+        clearInterval(timerId);
+        return;
+    };
+
+    const days = Math.floor(timeSpan / day);
+    const hours = Math.floor((timeSpan % day)/hour);
+    const mins = Math.floor((timeSpan % hour)/min);
+    const secs = Math.floor((timeSpan % min)/sec);
+
+    timeLeft.innerHTML = days + ' days ' + hours + ' hours ' + mins + ' minutes ' + secs + " seconds ";
+};
+
+timerId = setInterval(countDown,sec);
+
+console.log(birthday);
